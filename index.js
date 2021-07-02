4fea01f3c09281ddbeeb4c2125004c2e1993a38a
Shamialrahad <rahad15-7349@diu.edu.bd>
2021-07-02 16:04:55 +0600

    minimize code and delete duplicate code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+//price increase and decrease function
 function handleProductChange(product, isIncrease){
-    const productInput = document.getElementById(product+'-count');
-    const productCount = parseInt(productInput.value);
+    const productCount = getInputValue(product);
     let productNewCount = productCount;
     if(isIncrease == true){
         productNewCount = productCount + 1;
@@ -8,7 +8,7 @@ function handleProductChange(product, isIncrease){
     if(isIncrease == false && productCount > 0){
         productNewCount = productCount -1;
     }
-    productInput.value = productNewCount;
+    document.getElementById(product+'-count').value = productNewCount;
     let productTotal = 0;
     if(product == 'phone'){
         productTotal = productNewCount*1219;
@@ -16,8 +16,30 @@ function handleProductChange(product, isIncrease){
     if(product == 'case'){
         productTotal = productNewCount*59;
     }
-    document.getElementById(product+'-total').innerText = productTotal;
+    document.getElementById(product+'-total').innerText = '$'+productTotal;
+    calculateTotal();
 }
+
+function calculateTotal(){
+    const phoneCount = getInputValue('phone');
+    const caseCount = getInputValue('case');
+
+    const totalPrice = phoneCount*1219 + caseCount*59;
+    document.getElementById('total-price').innerText ='$'+totalPrice;
+
+    const tax = Math.round(totalPrice*0.1);
+    document.getElementById('tax-amount').innerText = '$' + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
+
 
 
 
